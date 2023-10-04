0f83671fb053d3967481720ceba38d5567a588ce
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-10-04 22:51:01 +0900

    act functions

---
+++ etc/actFunc.h
@@ -0,0 +1,34 @@
+/*
+ * actFunc.h
+ *
+ *  Created on: 2023. 9. 18.
+ *      Author: Sora Sugiyama
+ */
+
+#ifndef ACTFUNC_H_
+#define ACTFUNC_H_
+
+#include <cmath>
+#include "matrix.h"
+using namespace std;
+
+double Sigmoid(double x){
+	return 1.0/(1+exp(-x));
+}
+
+double ReLU(double x){
+	return x>0?x:0;
+}
+
+double LeakyReLU(double m,double x){
+	return x>0?x:x*m;
+}
+
+vecd Softmax(vecd z){
+	double sofsum=0.0;
+	for(double x:z)sofsum+=exp(x);
+	for(double &x:z)x/=sofsum;
+	return z;
+}
+
+#endif /* ACTFUNC_H_ */
